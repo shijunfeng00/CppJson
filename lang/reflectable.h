6724c73ebf2e25823e7fe3c27061f40d59b0f230
shijunfeng00 <38905022+shijunfeng00@users.noreply.github.com>
2021-11-14 18:58:22 +0800

    添加对于成员函数的反射支持

--- lang/reflectable.h
+++ lang/reflectable.h
@@ -9,29 +9,76 @@ public:
 	virtual ~Reflectable(){};
 	template<typename T,typename ...Args>
 	struct Regist;
+	
 	template<typename T>
 	struct Regist<T>;
+	
 	template<typename T>
 	static Config get_config(const T*object);//得到T的类型信息和名称,判断T类型的属性键值对是否建立,没有建立就建立(只建立一次) 
-	template<typename ClassType,typename FieldType>
-	static void set_field(ClassType&object,std::string field_name,const FieldType&data);//设置属性值，因为已经有类型信息，所以不需要调用default_constructors[type]里面的函数来构造 
-	template<typename FieldType=void*,typename ClassType>
-	static FieldType get_field(ClassType&object,std::string field_name); 
+	
 	template<typename FieldType=void*>
 	static FieldType get_field(void*object,std::string class_name,std::string field_name); 
-	static std::string&get_field_type(std::string class_name,std::string field_name); 
-	static void*get_instance(std::string class_name);
+
+	template<typename FieldType=void*,typename ClassType>
+	static FieldType get_field(ClassType&object,std::string field_name); 
+
+	static std::string get_field_type(std::string class_name,std::string field_name); 
+	
+	static std::vector<std::string>get_field_names();
+	static std::vector<std::string>get_method_names();
+	
+	template<typename ReturnType,typename ObjectType,typename...Args>
+	static auto get_method(ObjectType&object,const std::string&field_name,Args&&...args);
+	//通过字符串访问成员函数,get_field<返回值类型>(对象,字段名,参数列表...);
+
+	template<typename ClassType,typename FieldType>
+	static void set_field(ClassType&object,std::string field_name,const FieldType&data);//设置属性值，因为已经有类型信息，所以不需要调用default_constructors[type]里面的函数来构造 
+	
+	static void set_field(void*object,std::string class_name,std::string field_name,const auto&value);
+
 	static void delete_instance(std::string class_name,void*object); 
+	static void*get_instance(std::string class_name);
+	
 	using ClassName=std::string;
+	using MethodName=std::unordered_map<std::string,void(EmptyClass::*)(void*)>;
 	using FieldName=std::unordered_map<std::string,std::pair<std::string,std::size_t>>;
+//private:	
 	static std::unordered_map<ClassName,FieldName>field;
-private:	
 	static std::unordered_map<ClassName,std::function<void*(void)>>default_constructors;
 	static std::unordered_map<ClassName,std::function<void(void*)>>default_deconstructors;
+	static std::unordered_map<std::string,MethodName>&method;
 };
 std::unordered_map<std::string,std::function<void*(void)>>Reflectable::default_constructors;
 std::unordered_map<std::string,std::function<void(void*)>>Reflectable::default_deconstructors;
 std::unordered_map<std::string,std::unordered_map<std::string,std::pair<std::string,std::size_t>>>Reflectable::field;
+std::unordered_map<std::string,std::unordered_map<std::string,void(EmptyClass::*)(void*)>>&Reflectable::method=ConfigPair::from_classmethod_string;
+
+std::vector<std::string>Reflectable::get_field_names()
+{
+	static std::vector<std::string>names=[&]()->std::vector<std::string>
+	{
+		std::vector<std::string>names;
+		for(auto&it:field)
+		{
+			names.push_back(it.first);
+		}
+		return names;	
+	}();		
+	return names;
+}
+std::vector<std::string>Reflectable::get_method_names()
+{
+	static std::vector<std::string>names=[&]()->std::vector<std::string>
+	{
+		std::vector<std::string>names;
+		for(auto&it:method)
+		{
+			names.push_back(it.first);
+		}
+		return names;	
+	}();		
+	return names;
+}
 template<typename T>
 Config Reflectable::get_config(const T*object)
 {
@@ -40,12 +87,12 @@ Config Reflectable::get_config(const T*object)
 	config.update({{"class_name",class_name}});
 	return config;
 }
-template<typename ClassType,typename FieldType>
-void Reflectable::set_field(ClassType&object,std::string field_name,const FieldType&data)
+template<typename ReturnType,typename ObjectType,typename...Args>
+auto Reflectable::get_method(ObjectType&object,const std::string&field_name,Args&&...args)//通过字符串访问成员函数,get_field<返回值类型>(对象,字段名,参数列表...);
 {
-	std::string class_name=GET_TYPE_NAME(ClassType);
-	std::size_t offset=Reflectable::field[class_name][field_name].second;
-	*(FieldType*)((std::size_t)(&object)+offset)=data;
+	auto func=Reflectable::method[GET_TYPE_NAME(ObjectType)][field_name];
+	auto method=reinterpret_cast<ReturnType(ObjectType::*)(Args...)>(func);
+	return (object.*method)(std::forward<Args>(args)...);
 }
 template<typename FieldType=void*,typename ClassType>
 FieldType Reflectable::get_field(ClassType&object,std::string field_name)
@@ -64,9 +111,9 @@ FieldType Reflectable::get_field(void*object,std::string class_name,std::string 
 	if constexpr(std::is_same<FieldType,void*>::value)
 		return (void*)((std::size_t)(object)+offset);
 	else
-		return std::ref(*(FieldType*)((std::size_t)(&object)+offset));
+		return std::ref(*(FieldType*)((std::size_t)(object)+offset));
 }
-std::string&Reflectable::get_field_type(std::string class_name,std::string field_name)
+std::string Reflectable::get_field_type(std::string class_name,std::string field_name)
 {
 	return Reflectable::field[class_name][field_name].first;
 }
@@ -77,6 +124,19 @@ void*Reflectable::get_instance(std::string class_name)
 void Reflectable::delete_instance(std::string class_name,void*object)
 {
 	default_deconstructors[class_name](object);
+}
+template<typename ClassType,typename FieldType>
+void Reflectable::set_field(ClassType&object,std::string field_name,const FieldType&data)
+{
+	std::string class_name=GET_TYPE_NAME(ClassType);
+	std::size_t offset=Reflectable::field[class_name][field_name].second;
+	*(FieldType*)((std::size_t)(&object)+offset)=data;
+}
+void Reflectable::set_field(void*object,std::string class_name,std::string field_name,const auto&value)
+{
+	std::size_t offset=Reflectable::field[class_name][field_name].second;
+	using field_type=typename std::remove_const<typename std::remove_reference<decltype(value)>::type>::type;
+	*(field_type*)((std::size_t)(object)+offset)=value;
 }
 template<typename T,typename ...Args>
 struct Reflectable::Regist
