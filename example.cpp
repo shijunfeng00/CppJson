e7d0cd4dc3ddf4b335f2df0dd6642c6ab46954fd
shijunfeng00 <38905022+shijunfeng00@users.noreply.github.com>
2021-11-23 22:27:37 +0800

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,292 +1,68 @@
-#ifndef __SERIALIZABLE_H__
-#define __SERIALIZABLE_H__
 #include<iostream>
-#include<functional>
-#include<algorithm>
-#include"reflectable.h"
-#include"exception.h"
-class Serializable:public Reflectable
+#include"lang/serializable.h"
+#include<vector>
+using namespace std;
+struct Node
 {
-public: 
-	virtual ~Serializable(){};
-	template<typename T,typename ...Args>
-	struct Regist;
-	template<typename T>
-	struct Inherit;                                       //如果该类是子类,使用Serializable::Inherit<Father>::get_config(this).暂时没考虑多继承的问题
-	template<typename T>                                  //未来考虑采用Serializable::Inherit<FatherA,FatherB,FatherC>::get_config(this)来接受多继承问题
-	struct Regist<T>;
-	template<typename Object>
-	static Config get_config(const Object*object);        //Reflectable::get_config
-	template<typename Object>
-	static std::string dumps(const Object&object);        //序列化对象
-	template<typename Object=void*>
-	static auto loads(const std::string&json);            //反序列化还原对象
-	static Config decode(const std::string&serialized);   //从字符串中还原Config对象
-	template<typename Object>                             
-	static void from_config(Object*object,Config&config); //从Config中还原原始对象
-};
-template<typename Object>
-Config Serializable::get_config(const Object*object)
-{
-	return Reflectable::get_config(object);	
-}
-template<typename Object>
-void Serializable::from_config(Object*object,Config&config)
-{
-/*
-	std::string&class_name=config["class_name"];
-	class_name.erase(                                        
-		std::remove_if(class_name.begin(),class_name.end(),[](auto ch){return ch=='\"';}),//去掉两边的引号
-		class_name.end());
-	std::cout<<class_name<<" "<<GET_TYPE_NAME(Object)<<std::endl;
-	std::cout<<(class_name==std::string(GET_TYPE_NAME(Object)))<<std::endl<<std::endl;
-*/
-	std::string class_name=GET_TYPE_NAME(Object);
-	for(auto&it:config)
+	int x=1;
+	float y=5;
+	std::string z="sjf";
+	int add(int x,int y)
 	{
-		if(it.first!="class_name")
-		{
-			auto&field_name=it.first;
-			auto&value=it.second;
-			std::string type=Reflectable::get_field_type(class_name,field_name);     //得到类型名称
-			void*field=Reflectable::get_field(object,class_name,field_name);         //得到属性地址
-			if(type[type.size()-1]=='*'&&value=="null")                              //空指针
-				*(void**)field=nullptr;
-			else 
-				ConfigPair::from_config_string[type](field,value);                   //递归进行反序列化
-		}
+		return x+y;
 	}
-}
-template<typename Object>
-std::string Serializable::dumps(const Object&object)
-{
-	return object.get_config().serialized_to_string();
-}
-Config Serializable::decode(const std::string&serialized)                    
-{
-	constexpr int init=0;                                                          //定义各种状态
-	constexpr int parse_value=1;
-	constexpr int parse_struct=2;
-	constexpr int parse_fundamental=3;
-	constexpr int parse_iterable=4;
-	constexpr int parse_string=5;
-	constexpr int end_parse=6;
-	std::string key;
-	std::string value;
-	int nested_struct_layer=0;
-	int nested_iterable_layer=0;
-	int state=init;
-	int length=serialized.size();
-	Config config;
-	for(int i=0;i<length;++i)
+	std::string getName()
 	{
-		auto&it=serialized[i];
-		if(state==init)                                        //在冒号以前的字符为属性名
-		{
-			if(it==':')
-				state=parse_value;                             //冒号以后就是属性值对应的字符串
-			else if(it!='\"'&&it!='{'&&it!=','&&it!=' ')       //但是得排除两边的双引号
-				key.push_back(it);
-		}
-		else if(state==parse_value)                            //开始解析结果
-		{
-			if(it=='{')                                        //如果是大括号包起来的，那就是struct对象
-			{
-				value.push_back(it);
-				nested_struct_layer++;                         //{{{}}}嵌套,遇左大括号加1，又大括号-1,到0则结束
-				state=parse_struct;
-			}
-			else if(it=='[')                                   //列表,"[1,2,3,4,5]"
-			{
-				value.push_back(it);
-				nested_iterable_layer++;                       //可能遇到嵌套列表的情况
-				state=parse_iterable;
-			}
-			else if(it=='\"')
-			{
-				value.push_back(it);
-				state=parse_string;
-			}
-			else if(it!=' ')                                   //否则就是基本类型
-			{
-				value.push_back(it);
-				state=parse_fundamental;
-			}
-		}
-		else if(state==parse_string)
-		{
-			value.push_back(it);
-			if(it=='\"'&&serialized[i-1]!='\\')                // \" 转义字符不是结束.
-			{
-				state=end_parse;
-				--i;
-			}
-		}
-		else if(state==parse_fundamental) 
-		{
-			if(it==','||it=='}'||it=='\"')                     //遇到逗号结束,对于最后一个属性，遇到的会是大括号，对于字符串，遇到双引号
-			{
-				if(it=='\"')
-					value.push_back(it);                       //双引号需要算进值本身
-				state=end_parse;
-				--i;
-				continue;
-			}
-			value.push_back(it);
-		}
-		else if(state==parse_iterable)                          //可能有嵌套的情况
-		{
-			if(it==']'||it=='[')
-			{
-				nested_iterable_layer+=(it==']'?-1:1);
-				value.push_back(it);
-				if(nested_iterable_layer==0)
-				{
-					state=end_parse;
-					--i;
-				}
-				continue;
-			}
-			value.push_back(it);
-		}
-		else if(state==parse_struct)                             //遇到大括号结束
-		{
-			if(it=='}'||it=='{')
-			{
-				nested_struct_layer+=(it=='}'?-1:1);
-				value.push_back(it);
-				if(nested_struct_layer==0)
-				{
-					state=end_parse;
-					--i;
-				}
-				continue;
-			}
-			value.push_back(it);
-		}
-		else if(state==end_parse)                                //解析完一个属性对应的键值对，记录到config中
-		{
-			state=init;
-			config[key]=value;
-//			std::cout<<"<key>:"<<key<<"   <value>:"<<value<<std::endl;
-			key.clear();
-			value.clear();
-		}
+		return z;
 	}
-	if(!(state==end_parse&&nested_iterable_layer==0&&nested_struct_layer==0))
-	{   //不为零说明左右括号数量不匹配，说明字符串并不是合法的Json字串
-		if(nested_iterable_layer>0)
-			throw JsonDecodeDelimiterException(']');
-		else if(nested_iterable_layer<0)
-			throw JsonDecodeDelimiterException('[');
-		if(nested_struct_layer>0)
-			throw JsonDecodeDelimiterException('}');
-		else if(nested_struct_layer<0)
-			throw JsonDecodeDelimiterException('{');
-	}
-	return config;
-}
-template<typename Object=void*>
-auto Serializable::loads(const std::string&json)
-{
-	static_assert(!std::is_same<Object,void*>::value,"Not implemented yet.");
-	/*
-	Config config=Serializable::decode(json);                                             //从json字符串还原Config
-	std::string&class_name=config["class_name"];
-	class_name.erase(                                        
-		std::remove_if(class_name.begin(),class_name.end(),[](auto ch){return ch=='\"';}),//去掉两边的引号
-		class_name.end());
-	std::cout<<class_name<<" "<<GET_TYPE_NAME(Object)<<std::endl;
-	std::cout<<(class_name==std::string(GET_TYPE_NAME(Object)))<<std::endl<<std::endl;
-	*/
-	std::string class_name=GET_TYPE_NAME(Object);
-	void*object=nullptr;
-	try
-	{																																																																																																																																															
-		object=Reflectable::get_instance(class_name);                                         //创建实例
-		ConfigPair::from_config_string[class_name](object,json);                              //反序列化还原
-		if constexpr(std::is_same<Object,void*>::value)
-			return object;
-		else
-			return std::ref(*(Object*)object);	
-	}
-	catch(std::exception&e)                                                                   //在反序列化中由于错误的字段名或者不合法的Json字串导致解码失败
-	{      
-		Reflectable::delete_instance(class_name,object);                                      //暂时还没想好怎么去具体的检测是哪里出了什么问题，因此统一抛出一个Unknow异常.
-		throw JsonDecodeUnknowException();
-	}
-	
-}
-template<typename T,typename ...Args>
-struct Serializable::Regist
-{
-	Regist()
+	Config get_config()const
 	{
-		using Tptr=T*;
-		ConfigPair::from_config_string[GET_TYPE_NAME(T)]=[](void*object,const std::string&value)->void    //实质还是调用from_config进行递归.
-		{
-			Config config=Serializable::decode(value);
-			Serializable::from_config((T*)object,config);
-		};
-		ConfigPair::from_config_string[GET_TYPE_NAME(Tptr)]=[](void*object,const std::string&value)->void //对应的指针
-		{
-			Config config=Serializable::decode(value);
-			if(value=="null")
-			{
-				(*(Tptr*)object)=nullptr;
-			}
-			else
-			{
-		   		(*(Tptr*)object)=(T*)Reflectable::get_instance(GET_TYPE_NAME(T));
-				Serializable::from_config((*(Tptr*)object),config);
-			}
-		};
-		Reflectable::Regist<T>();
-		Serializable::Regist<Args...>();
-	}
-};
-template<typename T>
-struct Serializable::Regist<T>
-{
-	Regist()
-	{
-		using Tptr=T*;
-		ConfigPair::from_config_string[GET_TYPE_NAME(T)]=[](void*object,const std::string&value)->void   //实质还是调用from_config进行递归.
-		{
-			Config config=Serializable::decode(value);
-			Serializable::from_config((T*)object,config);
-		};
-		ConfigPair::from_config_string[GET_TYPE_NAME(Tptr)]=[](void*object,const std::string&value)->void //对应的指针
-		{
-			Config config=Serializable::decode(value);
-			if(value=="null")
-			{
-				(*(Tptr*)object)=nullptr;
-			}
-			else
-			{
-		   		(*(Tptr*)object)=(T*)Reflectable::get_instance(GET_TYPE_NAME(T));
-				Serializable::from_config((*(Tptr*)object),config);
-			}
-		};
-		Reflectable::Regist<T>();
-	}
-};
-template<typename Parent>
-struct Serializable::Inherit
-{
-	template<typename Object>
-	static Config get_config(const Object*object)
-	{
-		Config parent_config=object->Parent::get_config();
-		auto sub_config=Reflectable::field.find(GET_TYPE_NAME(Object));
-		if(sub_config==Reflectable::field.end())
-			field[GET_TYPE_NAME(Object)]=Reflectable::field[GET_TYPE_NAME(Parent)];
-		Config config=Serializable::get_config(object);
-		for(auto&it:parent_config)
-			if(it.first!="class_name")
-				config[it.first]=it.second;
+		Config config=Reflectable::get_config(this);
+		config.update({
+			{"x",x},
+			{"y",y},
+			{"z",z},
+			{"add",add},
+			{"getName",getName}
+		});
 		return config;
 	}
 };
-#endif
+int main()
+{
+	Serializable::Regist<Node>();
+	void*object=Reflectable::get_instance("Node");                        //创建实例
+
+	Reflectable::set_field<int>(object,"Node","x",4);                     //通过字符串名称修改成员变量
+	Reflectable::set_field<float>(object,"Node","y",5);
+	Reflectable::set_field<std::string>(object,"Node","z","test");
+	int field_x=Reflectable::get_field<int>(object,"Node","x");           //通过字符串名称得到值
+	float field_y=Reflectable::get_field<float>(object,"Node","y");
+	string field_z=Reflectable::get_field<string>(object,"Node","z");
+	
+	cout<<field_x<<" "<<(*(Node*)object).x<<endl;                         //正常访问成员变量,对比结果
+	cout<<field_y<<" "<<(*(Node*)object).y<<endl;
+	cout<<field_z<<" "<<(*(Node*)object).z<<endl;
+	/*序列化与反序列化*/
+	
+	std::string json=Serializable::dumps(*(Node*)object);                 //序列化
+	cout<<json<<endl;
+	Node b=Serializable::loads<Node>(json);                               //反序列化
+	/*正常访问*/
+	cout<<b.x<<endl;                                                      //正常访问成员变量
+	cout<<b.y<<endl;
+	cout<<b.z<<endl;
+	/*成员函数反射*/
+	cout<<Reflectable::get_method<int>(b,"add",5,6)<<endl;                //通过字符串名称访问成员函数
+	cout<<Reflectable::get_method<string>(b,"getName")<<endl;
+}
+/*
+4 4
+5 5
+test test
+{ "z":"test", "y":5, "x":4, "class_name":"Node" }
+4
+5
+test
+11
+*/
