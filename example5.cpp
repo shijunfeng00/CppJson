1fbba3a66b957285de4f4fd09d264b02265328bc
shijunfeng00 <38905022+shijunfeng00@users.noreply.github.com>
2021-11-24 21:43:07 +0800

    int,std::vector<int>等非自定义struct/class 类型的序列化与反序列化
    比如说：
    Serializable::dumps({1,2,3,4,5,6,7});
    Serializable::dumps(4.5f);
    Serializable::dumps(make_pair(1,string{"test"}));
    而之前只能传入实现了Config get_config()const;方法的struct/class

--- example5.cpp
+++ example5.cpp
@@ -10,20 +10,25 @@ int main()
 	{
 		auto object=std::tuple<int,std::vector<int>>{5,{4,5,6}};
 		string json=Serializable::dumps(object);
-		cout<<json<<endl;
 		decltype(object) &p=Serializable::loads<decltype(object)>(json);
+		cout<<json<<endl;
 		cout<<get<0>(p)<<endl;
-		cout<<get<1>(p)[0]<<" "<<get<1>(p)[1]<<" "<<get<1>(p)[2];
+		cout<<get<1>(p)[0]<<" "<<get<1>(p)[1]<<" "<<get<1>(p)[2]<<endl;
+		cout<<"----------\n";
+		json=Serializable::dumps(2.718f);
+		float&e=Serializable::loads<float>(json);
+		cout<<json<<endl;
+		cout<<e<<endl;
+		cout<<"----------\n";
+		json=Serializable::dumps({1,1,2,3,5,8});
+		std::vector<int>&f1=Serializable::loads<std::vector<int>>(json);
+		cout<<json<<endl;
+		for(auto&it:f1)
+			cout<<it<<",";
+		cout<<endl<<endl;
 	}
 	catch(std::exception&e)
 	{
 		cout<<e.what();
 	}
 }
-/*
-output:
-[5,[4,5,6]]
-std::vector<int, std::allocator<int> >
-5
-4 5 6
-*/
