02da599e30dc58b7f501885fbd11fe872122342b
shijunfeng00 <38905022+shijunfeng00@users.noreply.github.com>
2021-12-19 13:35:29 +0800

    int,std::vector<int>等非自定义struct/class 类型的序列化与反序列化

--- example5.cpp
+++ example5.cpp
@@ -10,18 +10,18 @@ int main()
 	{
 		auto object=std::tuple<int,std::vector<int>>{5,{4,5,6}};
 		string json=Serializable::dumps(object);
-		decltype(object) &p=Serializable::loads<decltype(object)>(json);
+		decltype(object) p=Serializable::loads<decltype(object)>(json);
 		cout<<json<<endl;
 		cout<<get<0>(p)<<endl;
 		cout<<get<1>(p)[0]<<" "<<get<1>(p)[1]<<" "<<get<1>(p)[2]<<endl;
 		cout<<"----------\n";
 		json=Serializable::dumps(2.718f);
-		float&e=Serializable::loads<float>(json);
+		float e=Serializable::loads<float>(json);
 		cout<<json<<endl;
 		cout<<e<<endl;
 		cout<<"----------\n";
 		json=Serializable::dumps({1,1,2,3,5,8});
-		std::vector<int>&f1=Serializable::loads<std::vector<int>>(json);
+		std::vector<int>f1=Serializable::loads<std::vector<int>>(json);
 		cout<<json<<endl;
 		for(auto&it:f1)
 			cout<<it<<",";
